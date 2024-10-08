8b3dcf40395850a6f39fb2679f65ad5bf7c9a6d9
TheMhv <mhv.geral@gmail.com>
2024-10-08 10:32:00 -0300

    Fix: Nost client cache connection

--- src/lib/nostr/client.ts
+++ src/lib/nostr/client.ts
@@ -1,22 +1,48 @@
 import { Client, loadWasmAsync } from "@rust-nostr/nostr-sdk";
 
-let cachedClient: Client | null = null;
+class NostrClientManager {
+  private static instance: NostrClientManager;
+  private client: Client | null = null;
+  private initializationPromise: Promise<Client> | null = null;
 
-export async function clientConnect() {
-  if (cachedClient) {
-    return cachedClient;
+  private constructor() {}
+
+  public static getInstance(): NostrClientManager {
+    if (!NostrClientManager.instance) {
+      NostrClientManager.instance = new NostrClientManager();
+    }
+    return NostrClientManager.instance;
   }
 
-  await loadWasmAsync();
+  public async getClient(): Promise<Client> {
+    if (this.client) {
+      return this.client;
+    }
 
-  const client = new Client();
+    if (!this.initializationPromise) {
+      this.initializationPromise = this.initializeClient();
+    }
 
-  await client.addRelay("wss://relay.snort.social");
-  await client.addRelay("wss://nos.lol");
-  await client.addRelay("wss://relay.damus.io");
-  await client.addRelay("wss://nostr.wine");
-  await client.connect();
+    return this.initializationPromise;
+  }
 
-  cachedClient = client;
-  return client;
+  private async initializeClient(): Promise<Client> {
+    await loadWasmAsync();
+
+    const client = new Client();
+
+    await client.addRelay("wss://relay.snort.social");
+    await client.addRelay("wss://nos.lol");
+    await client.addRelay("wss://relay.damus.io");
+    await client.addRelay("wss://nostr.wine");
+    await client.connect();
+
+    this.client = client;
+    return client;
+  }
+}
+
+export async function clientConnect(): Promise<Client> {
+  const manager = NostrClientManager.getInstance();
+  return manager.getClient();
 }
