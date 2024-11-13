bfb4c491181678499460d4957b4e40a230ddba66
TheMhv <mhv.geral@gmail.com>
2024-11-13 19:45:04 -0300

    Feat: Create profile badge and NostrProvider component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import { Card } from "@/components/ui/card";
 import Logo from "@/components/logo";
 import Link from "next/link";
+import CornerMenu from "@/components/cornerMenu/cornerMenu";
+import { NostrProvider } from "@/components/NostrProvider";
 
 export const metadata: Metadata = {
   title: "Livetoshi",
@@ -16,20 +18,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="antialiased">
-        <div className="flex items-center justify-center min-h-screen font-sans">
-          <div>
-            {children}
+    <NostrProvider>
+      <html lang="en">
+        <body className="antialiased relative">
+          <CornerMenu />
 
-            <Link href="https://github.com/TheMhv/Livetoshi">
-              <Card className="backdrop-invert backdrop-blur-lg hover:scale-95 px-2 py-1 max-w-fit mx-auto my-5">
-                <Logo />
-              </Card>
-            </Link>
+          <div className="flex items-center justify-center min-h-screen font-sans">
+            <div>
+              {children}
+
+              <Link href="https://github.com/TheMhv/Livetoshi">
+                <Card className="backdrop-invert backdrop-blur-lg hover:scale-95 px-2 py-1 max-w-fit mx-auto my-5">
+                  <Logo />
+                </Card>
+              </Link>
+            </div>
           </div>
-        </div>
-      </body>
-    </html>
+        </body>
+      </html>
+    </NostrProvider>
   );
 }
